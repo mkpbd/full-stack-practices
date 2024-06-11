f6ec76b7bbc8d6323c544a045b73700f7d6ce564
mkpbd <mostofakamal1990@gmail.com>
2024-06-11 19:41:50 +0600

    node  express, monogo project bootstraping

---
+++ src/app/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+// import { AdminRoutes } from '../modules/Admin/admin.route';
+
+
+const router = Router();
+
+// const moduleRoutes = [
+//   {
+//     path: '/users',
+//      route: 'give your module Name',
+//     // route: UserRoutes,
+//   }
+// ];
+
+//  moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+
+export default router;
